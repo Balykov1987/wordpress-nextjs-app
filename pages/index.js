3850c7506ed31a27bf7c11540fa8fe7e8e1c3c52
Anton Balykov <Balykov1987@gmail.com>
2021-12-15 12:01:12 +0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home( {page} ){
+  // console.log({page});
+
+  
+  return(
+    <div>
+    <h1>{page.title}</h1>
+    <div>
+      {page.content}
+    </div>
+    <Image width={640} height={400} src={page.featuredImage.node.sourceUrl} alt={page.title}/>
+    </div>
+  )
+}
+
+export async function getStaticProps(){
+
+  const res = await fetch('https://suncoat-of-texas.directline.company/graphql',{
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      query:`
+      query MyQuery {
+        page(id: "about", idType: URI) {
+          slug
+          title
+          content
+          featuredImage {
+            node {
+              id
+              sourceUrl
+            }
+          }
+        }
+      }
+      `,
+    })
+  })
+
+  const json = await res.json()
+
+  return {
+    props: {
+      page: json.data.page,
+    }
+  }
+
+}
